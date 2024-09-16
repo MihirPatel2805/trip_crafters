b950ccb2448d4a4ae3db1609ab9ea40d7eca6790
Patel Mihir Jitendrabhai <mihirpatel3780@gmail.com>
2024-09-16 16:32:37 +0530

    delete app trains and bookings

--- backend/TripCrafter/trains/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TrainListView, StationListView, TrainDetailView
-
-urlpatterns = [
-    path('trains/', TrainListView.as_view(), name='train-list'),
-    path('stations/', StationListView.as_view(), name='station-list'),
-    path('trains/<str:number>/', TrainDetailView.as_view(), name='train-detail'),
-]
